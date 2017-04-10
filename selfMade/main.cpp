0c97e46dbf579e0bd390867cb4ef072d192b7e49
GotterHHH <yhanag@ust.hk>
2017-04-10 22:38:56 +0800

    Initial Commit

---
+++ selfMade/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  selfMade
+//
+//  Created by 客人用户 on 10/4/17.
+//  Copyright © 2017年 yhanag. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
